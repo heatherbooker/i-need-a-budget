2e0d9f8e6010ed81c47fb7995550bac3da58a970
Heather Booker <heather.booker@adgear.com>
2018-04-27 13:50:09 -0400

    Always drop and recreate db

--- create-db.py
+++ create-db.py
@@ -2,22 +2,16 @@ import psycopg2
 
 # TALK TO DA DB
 conn = psycopg2.connect(dbname='postgres')
-conn.autocommit = True # Commit the changes we make
+conn.autocommit = True # Must be in autocommit to create db
+autocommitcur = conn.cursor()
+
+autocommitcur.execute('DROP DATABASE IF EXISTS test_budget')
+autocommitcur.execute('CREATE DATABASE test_budget')
+conn.autocommit = False
+
 cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS expenses (date DATE NOT NULL DEFAULT CURRENT_DATE)")
 
-# create or remove?
-while True:
-    create_or_drop = raw_input('create db or drop db?')
-    if create_or_drop in ('c', 'create'):
-        cur.execute('CREATE DATABASE IF NOT EXISTS test_budget')
-        conn.autocommit = False
-        cur.execute("CREATE TABLE IF NOT EXISTS expenses (date DATE NOT NULL DEFAULT 'today')")
-        break
-    elif create_or_drop in ('d', 'drop'):
-        cur.execute('DROP DATABASE test_budget')
-        break
-    else:
-        print 'nah son, try "create" or "drop"'
-
+conn.commit()
 conn.close()
 
