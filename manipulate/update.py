39803fe2c7004a96f02d551bb9b994e438a43b33
Heather Booker <heather.booker@hotmail.com>
2018-06-23 17:26:52 -0400

    Use yaml and wrap stuff in happy lil functions

--- manipulate/update.py
+++ manipulate/update.py
@@ -2,53 +2,9 @@ import calendar
 from datetime import date
 import psycopg2 as psy
 import sys
+import yaml
 
-which_db = 'test_budget'
-if len(sys.argv) > 1 and sys.argv[1] == '--fo-real':
-  which_db = 'budget'
-
-print('Using db: `{}`; use arg `--fo-real` to use `budget`.'.format(which_db))
-conn = psy.connect('dbname={}'.format(which_db))
-cur = conn.cursor()
-
-year = date.today().year
-
-month = 0
-while month not in range(1, 12):
-    month = int(input('What month (1-12) in {0} do you want to update categories expenses for? (current month: {1})'.format(year, date.today().month)))
-
-last_day = calendar.monthrange(year, month)[1]
-
-# TODO: put this in a separate file, not tracked by git
-categories = {
-  'category': ('subcat', 'subcat2', 'subcat3', 'subcat4'),
-  'othercategory': ('subcat5',)
-}
-data = {
-  'subcat': {
-    'amount': 12,
-    'day': 1
-  },
-  'subcat2': {
-    'amount': 22,
-    'day': 19
-  },
-  'subcat3': {
-    'amount': 3,
-    'day': 19
-  },
-  'subcat4': {
-    'amount': 20,
-    'day': 15
-  },
-  'subcat5': {
-    'amount': 400000,
-    'day': last_day
-  }
-}
-expenses_in_db = {}
-
-base_sql = """
+BASE_SQL = """
   SELECT subcategory, date, amount
   FROM expenses
   WHERE category = '{3}'
@@ -56,17 +12,63 @@ base_sql = """
   AND date between '{0}-{1}-01' and '{0}-{1}-{2}'
 """
 
-for category in categories:
-  sql = base_sql.format(year, str(month).zfill(2), str(last_day).zfill(2), category)
-  cur.execute(sql, (categories[category],))
-  for row in cur:
-    expenses_in_db[row[0]] = row
-  for subcat in categories[category]:
-    if subcat in expenses_in_db:
-      continue
-    insert = raw_input('Would you like to insert for {0}?'.format(subcat))
-    if insert.lower() in ('y', 'yes'):
-      cur.execute("INSERT INTO expenses (date, amount, category, subcategory) VALUES ('{}-{}-{}', {}, '{}', '{}')".format(year, month, str(data[subcat]['day']).zfill(2), data[subcat]['amount'], category, subcat))
+def connect_to_db():
+  which_db = 'test_budget'
+  if len(sys.argv) > 1 and sys.argv[1] == '--fo-real':
+    which_db = 'budget'
 
-conn.commit()
-conn.close()
+  print('Using db: `{}`; use arg `--fo-real` to use `budget`.'.format(which_db))
+  conn = psy.connect('dbname={}'.format(which_db))
+  return conn
+
+
+def get_dates():
+  year = date.today().year
+
+  month = 0
+  while month not in range(1, 12):
+      month = int(input('What month (1-12) in {0} do you want to update categories expenses for? (current month: {1})'.format(year, date.today().month)))
+
+  last_day = str(calendar.monthrange(year, month)[1]).zfill(2)
+
+  return { 'year': year, 'month': str(month).zfill(2), 'last_day': last_day }
+
+def insert_recurring(categories, details, dates, cur):
+  expenses_in_db = {}
+  year = dates['year']
+  month = dates['month']
+  last_day = dates['last_day']
+
+  for category in categories:
+    sql = BASE_SQL.format(year, month, last_day, category)
+    cur.execute(sql, (tuple(categories[category]),))
+    for row in cur:
+      expenses_in_db[row[0]] = row
+    for subcat in categories[category]:
+      if subcat in expenses_in_db:
+        continue
+      insert = raw_input('Would you like to insert for {0}?'.format(subcat))
+      if insert.lower() in ('y', 'yes'):
+        print('Inserting for {0}'.format(subcat))
+        day = str(details[subcat]['day']).zfill(2)
+        amount = details[subcat]['amount']
+        cur.execute("INSERT INTO expenses (date, amount, category, subcategory) VALUES ('{}-{}-{}', {}, '{}', '{}')".format(year, month, day, amount, category, subcat))
+
+def close_db(conn):
+  conn.commit()
+  conn.close()
+
+with open('data.yml', 'r') as stream:
+  info = yaml.load(stream)
+  details = info['details']
+  categories = info['categories']
+
+  conn = connect_to_db()
+  cur = conn.cursor()
+
+  dates = get_dates()
+
+  insert_recurring(categories, details, dates, cur)
+
+  close_db(conn)
+sys.exit()
