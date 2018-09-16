d0327fe976bfa4a9fec3a7e84ad07aca6046bde0
Heather Booker <heather.booker@hotmail.com>
2018-09-16 10:50:43 -0400

    Wrap work in functions

--- analyze/category.py
+++ analyze/category.py
@@ -11,27 +11,30 @@ def omit(from_list):
     else:
         return from_list
 
-# TALK TO DA DB
-conn = psycopg2.connect('dbname=budget')
-cur = conn.cursor()
+def get_data():
+    conn = psycopg2.connect('dbname=budget')
+    cur = conn.cursor()
 
-cur.execute('SELECT DISTINCT category FROM expenses')
-categories = cur.fetchall()
-categories = map((lambda tupl: tupl[0]), categories)
-categories = omit(categories)
+    cur.execute('SELECT DISTINCT category FROM expenses')
+    categories = cur.fetchall()
+    categories = map((lambda tupl: tupl[0]), categories)
+    categories = omit(categories)
 
-amounts = []
-for category in categories:
-  cur.execute('SELECT SUM(amount) FROM expenses WHERE category = %s', (category,))
-  total = cur.fetchone()
-  amounts.append(total)
+    amounts = []
+    for category in categories:
+      cur.execute('SELECT SUM(amount) FROM expenses WHERE category = %s', (category,))
+      total = cur.fetchone()
+      amounts.append(total)
 
-conn.close()
+    conn.close()
 
+    return [categories, amounts]
 
-# SHOW ME THE MONEY (lol u so funny)
-colors = cm.tab20b(np.linspace(0, 1, len(categories)))
-plt.pie(amounts, labels=categories, colors=colors)
-plt.axis('equal')
-plt.show()
+def show_me_the_money(categories, amounts):
+    colors = cm.tab20b(np.linspace(0, 1, len(categories)))
+    plt.pie(amounts, labels=categories, colors=colors)
+    plt.axis('equal')
+    plt.show()
 
+[categories, amounts] = get_data()
+show_me_the_money(categories, amounts)
