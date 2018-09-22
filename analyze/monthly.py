8baf3ffbeada7ad939d473d8848fa0e4c0f45dcc
Heather Booker <heather.booker@hotmail.com>
2018-09-21 20:53:10 -0400

    Can analyze monthly by category

--- analyze/monthly.py
+++ analyze/monthly.py
@@ -1,14 +1,20 @@
 import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import numpy as np
+import sys
 
 conn = psycopg2.connect('dbname=budget')
 
-df = pd.read_sql("SELECT date, amount from expenses",
-        conn, parse_dates=['date'], index_col='date')
+def get_df():
+    if len(sys.argv) > 1 and sys.argv[1] == '--category':
+        args = tuple(map((lambda x: x.replace('_', ' ')), sys.argv[2:]))
+        return pd.read_sql("SELECT date, amount FROM expenses WHERE category in (%s)", conn, params=args, parse_dates=['date'], index_col='date')
+    else:
+        return pd.read_sql("SELECT date, amount FROM expenses",
+                    conn, parse_dates=['date'], index_col='date')
 
+
+df = get_df()
 
 # Sum by month and plot
 df.resample("M").sum().plot()
