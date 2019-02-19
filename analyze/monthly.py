cd193c4714aa19ceb4a9bd564759ea4d3d2dcef7
Heather Booker <heatherbooker@users.noreply.github.com>
2019-02-18 19:18:06 -0500

    Use argparse in analyze/monthly too

--- analyze/monthly.py
+++ analyze/monthly.py
@@ -2,19 +2,26 @@ import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import argparse
 
 conn = psycopg2.connect('dbname=budget')
 
-def get_df():
-    if len(sys.argv) > 1 and sys.argv[1] == '--category':
-        args = tuple(map((lambda x: x.replace('_', ' ')), sys.argv[2:]))
-        return pd.read_sql("SELECT date, amount FROM expenses WHERE category in (%s)", conn, params=args, parse_dates=['date'], index_col='date')
+def parse():
+    parser = argparse.ArgumentParser(description='analyze spending by month')
+    parser.add_argument('--category', nargs='*', help='specify which categories to analyze')
+    return parser.parse_args()
+
+def get_df(args):
+    if args.category:
+        categories = tuple(map((lambda x: x.replace('_', ' ')), args.category))
+        return pd.read_sql("SELECT date, amount FROM expenses WHERE category in (%s)", conn, params=categories, parse_dates=['date'], index_col='date')
     else:
         return pd.read_sql("SELECT date, amount FROM expenses",
                     conn, parse_dates=['date'], index_col='date')
 
 
-df = get_df()
+args = parse()
+df = get_df(args)
 
 # Sum by month and plot
 df.resample("M").sum().plot()
